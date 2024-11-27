7c560a81c95b66540dee8ad141dd0afbe12d867f
unknown <macarrion44@utpl.edu.ec>
2024-11-27 11:26:32 -0500

    cambios

--- EjemplosDoWhile/Ejemplos/src/ejemplos02/Ejemplo01.java
+++ EjemplosDoWhile/Ejemplos/src/ejemplos02/Ejemplo01.java
@@ -23,12 +23,14 @@ public class Ejemplo01 {
     public static void main(String[] args) {
         int contador = 1;
         int suma;
+        String cadena = "";
         suma = 0;
         do {
             suma = suma + contador;
+            System.out.printf("%s%d\n",cadena,contador);
             contador = contador + 1;
         } while (contador <= 10); 
-        System.out.printf("%d\n", suma);
+        System.out.printf("\n%d\n", suma);
     }
 
 }
